019775b0de9f9771f99d2bec196115b429aea1cb
Xyncross1111 <anasmk2003@gmail.com>
2026-05-09 13:02:50 +0200

    fix

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <>
       <HeroSection />
       <ImpactStats />
-      <MissionSection />
       <CausesPreview />
+      <MissionSection />
       <VolunteerTeam />
       <StatsBar />
       <Testimonials />
